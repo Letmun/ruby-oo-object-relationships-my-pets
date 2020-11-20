760c69c1e2f74cb84c7d731fb55d9532e4afeec0
Duncan Stark <letmunplaysgamesGamil.com>
2020-11-20 10:00:59 -0600

    commit tree 2baaeb34cf23c42aedca4445545cf54913cf74e5

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
-  # code goes here
+ @@Owners = []
+ 
+attr_reader :name, :species
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@Owners << self  
+  end 
+
+  def say_species
+    p "I am a #{self.species}."
+  end 
+
+  def self.all
+    p @@Owners
+  end
+  
+  def self.count
+    p self.all.count 
+  end 
 end
